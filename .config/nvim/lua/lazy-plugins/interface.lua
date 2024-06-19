34b91cd1ef16ba6ccac60e2eef4f5862d3c709ae
Cody Coons <cody@codycoons.com>
2024-06-18 23:19:30 -0400

    add whick-key

--- .config/nvim/lua/lazy-plugins/interface.lua
+++ .config/nvim/lua/lazy-plugins/interface.lua
@@ -141,11 +141,27 @@ return {
 
   {
     "nvim-telescope/telescope.nvim",
+    lazy = false,
+    event = "VeryLazy",
     opts = {},
     keys = {
       { "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
       { "<leader>,", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>", desc = "Switch Buffers" },
       { "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Grep" },
+    },
+  },
+
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
     },
   },
 }
