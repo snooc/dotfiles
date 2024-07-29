f20b0c717e5d8ef926ddedbc16327d12fa57c92e
Cody Coons <cody@codycoons.com>
2024-07-29 13:07:51 -0400

    wezterm

--- dot-config/wezterm/wezterm.lua
+++ dot-config/wezterm/wezterm.lua
@@ -1,46 +1,54 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-local padding = "20"
-local opacity = 0.85
-local transparent_bg = "rgba(22, 24, 26, " .. opacity .. ")"
-
-config.font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Regular", italic = false })
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono", {
+	weight = "Regular",
+	italic = false,
+})
 config.font_size = 14
 
-config.initial_rows = 45
+config.color_scheme = "Cyberdream"
+
+config.initial_rows = 50
 config.initial_cols = 180
-config.window_padding = { left = padding, right = padding, top = padding, bottom = padding }
-config.window_decorations = "RESIZE"
-config.window_background_opacity = opacity
+config.window_padding = {
+	left = 20,
+	right = 20,
+	top = 60,
+	bottom = 0,
+}
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.window_background_opacity = 0.85
+config.text_background_opacity = 0.85
 config.macos_window_background_blur = 20
 config.window_close_confirmation = "NeverPrompt"
 config.max_fps = 144
 config.animation_fps = 60
 config.cursor_blink_rate = 250
 
-config.colors = require("cyberdream")
-
 config.enable_tab_bar = true
-config.hide_tab_bar_if_only_one_tab = true
+config.hide_tab_bar_if_only_one_tab = false
 config.use_fancy_tab_bar = false
-config.colors.tab_bar = {
-	background = transparent_bg,
-	active_tab = {
-		bg_color = transparent_bg,
-		fg_color = "#ffffff",
+config.tab_bar_at_bottom = true
+
+-- timeout_milliseconds defaults to 1000 and can be omitted
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+	{
+		key = "%",
+		mods = "LEADER|SHIFT",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
 	},
-	inactive_tab = {
-		bg_color = transparent_bg,
-		fg_color = "#7b8496",
+	{
+		key = '"',
+		mods = "LEADER|SHIFT",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
 	},
-	inactive_tab_hover = {
-		bg_color = transparent_bg,
-		fg_color = "#ffffff",
-	},
-	new_tab = {
-		bg_color = transparent_bg,
-		fg_color = "#ffffff",
+	-- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+	{
+		key = "a",
+		mods = "LEADER|CTRL",
+		action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
 	},
 }
 
