894295b5c8ad016c574c41234f9d9e1f3cf64f97
Cody Coons <cody@codycoons.com>
2024-12-20 10:41:12 -0500

    move to fzf-lua

--- dot-config/nvim/lua/plugins/telescope.lua
+++ dot-config/nvim/lua/plugins/telescope.lua
@@ -1,38 +1,15 @@
 return {
   {
-    "nvim-telescope/telescope.nvim",
-    opts = {
-      defaults = {
-        hidden = true,
-
-        -- borderchars = { " ", " ", "", " ", " ", " ", " ", " " },
-        layout_strategy = "horizontal",
-        sorting_strategy = "ascending",
-
-        prompt_prefix = "   ",
-        selection_caret = "  ",
-        entry_prefix = "  ",
-
-        layout_config = {
-          horizontal = {
-            prompt_position = "top",
-            preview_width = 0.55,
-            results_width = 0.8,
-          },
-          vertical = {
-            mirror = false,
-          },
-          width = 0.87,
-          height = 0.80,
-          preview_cutoff = 120,
-        },
-      },
-    },
+    "ibhagwan/fzf-lua",
+    cmd = "FzfLua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {},
+    config = function(_, opts) require("fzf-lua").setup(opts) end,
     keys = {
-      { "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-      { "<leader>fb", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>", desc = "Switch Buffers" },
-      { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Grep" },
-      { "<leader>fh", "<cmd>Telescope highlights<cr>", desc = "Highlights" },
+      { "<leader><space>", "<cmd>FzfLua files<cr>", desc = "Find Files" },
+      { "<leader>fb", "<cmd>FzfLua buffers<cr>", desc = "Switch Buffers" },
+      { "<leader>fg", "<cmd>FzfLua live_grep<cr>", desc = "Grep" },
+      { "<leader>fh", "<cmd>FzfLua highlights<cr>", desc = "Highlights" },
     },
   },
 }
